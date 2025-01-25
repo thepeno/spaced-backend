2e3412666e7a71cc470724e6e1450b712778e7ed
zsheng <zsheng.dev@gmail.com>
2025-01-25 13:03:16 -0500

    Add server2client operations

---
+++ src/server2client.ts
@@ -0,0 +1,148 @@
+import { DB } from '@/db';
+import * as schema from '@/db/schema';
+import {
+	CardContentOperation,
+	CardDeletedOperation,
+	CardOperation,
+	DeckOperation,
+	Operation,
+	ServerToClient,
+	UpdateDeckCardOperation,
+} from '@/operation';
+import { and, gt, ne } from 'drizzle-orm';
+
+async function getCardsFromSeqNo(db: DB, requestingClientId: string, seqNo: number): Promise<ServerToClient<CardOperation>[]> {
+	const cards = await db.query.cards.findMany({
+		where: and(gt(schema.cards.seqNo, seqNo), ne(schema.cards.lastModifiedClient, requestingClientId)),
+		columns: {
+			seqNo: true,
+			lastModified: true,
+			id: true,
+		},
+	});
+
+	return cards.map((card) => ({
+		type: 'card',
+		seqNo: card.seqNo,
+		timestamp: card.lastModified.getTime(),
+		payload: {
+			id: card.id,
+		},
+	}));
+}
+
+async function getCardContentFromSeqNo(db: DB, requestingClientId: string, seqNo: number): Promise<ServerToClient<CardContentOperation>[]> {
+	const cardContents = await db.query.cardContents.findMany({
+		where: and(gt(schema.cardContents.seqNo, seqNo), ne(schema.cardContents.lastModifiedClient, requestingClientId)),
+		columns: {
+			seqNo: true,
+			lastModified: true,
+			cardId: true,
+			front: true,
+			back: true,
+		},
+	});
+
+	return cardContents.map((cardContent) => ({
+		type: 'cardContent',
+		seqNo: cardContent.seqNo,
+		timestamp: cardContent.lastModified.getTime(),
+		payload: {
+			cardId: cardContent.cardId,
+			front: cardContent.front,
+			back: cardContent.back,
+		},
+	}));
+}
+
+async function getCardDeletedFromSeqNo(db: DB, requestingClientId: string, seqNo: number): Promise<ServerToClient<CardDeletedOperation>[]> {
+	const cardDeleted = await db.query.cardDeleted.findMany({
+		where: and(gt(schema.cardDeleted.seqNo, seqNo), ne(schema.cardDeleted.lastModifiedClient, requestingClientId)),
+		columns: {
+			seqNo: true,
+			lastModified: true,
+			cardId: true,
+			deleted: true,
+		},
+	});
+
+	return cardDeleted.map((cardDeleted) => ({
+		type: 'cardDeleted',
+		seqNo: cardDeleted.seqNo,
+		timestamp: cardDeleted.lastModified.getTime(),
+		payload: {
+			cardId: cardDeleted.cardId,
+			deleted: cardDeleted.deleted,
+		},
+	}));
+}
+
+async function getDeckFromSeqNo(db: DB, requestingClientId: string, seqNo: number): Promise<ServerToClient<DeckOperation>[]> {
+	const decks = await db.query.decks.findMany({
+		where: and(gt(schema.decks.seqNo, seqNo), ne(schema.decks.lastModifiedClient, requestingClientId)),
+		columns: {
+			seqNo: true,
+			lastModified: true,
+			id: true,
+			name: true,
+			description: true,
+			deleted: true,
+		},
+	});
+
+	return decks.map((deck) => ({
+		type: 'deck',
+		seqNo: deck.seqNo,
+		timestamp: deck.lastModified.getTime(),
+		payload: {
+			id: deck.id,
+			name: deck.name,
+			description: deck.description,
+			deleted: deck.deleted,
+		},
+	}));
+}
+
+async function getDeckCardFromSeqNo(db: DB, requestingClientId: string, seqNo: number): Promise<ServerToClient<UpdateDeckCardOperation>[]> {
+	const deckCards = await db.query.cardDecks.findMany({
+		where: and(gt(schema.cardDecks.seqNo, seqNo), ne(schema.cardDecks.lastModifiedClient, requestingClientId)),
+		columns: {
+			seqNo: true,
+			lastModified: true,
+			cardId: true,
+			deckId: true,
+			clCount: true,
+		},
+	});
+
+	return deckCards.map((deckCard) => ({
+		type: 'updateDeckCard',
+		seqNo: deckCard.seqNo,
+		timestamp: deckCard.lastModified.getTime(),
+		payload: {
+			cardId: deckCard.cardId,
+			deckId: deckCard.deckId,
+			clCount: deckCard.clCount,
+		},
+	}));
+}
+
+/**
+ * Get all operations from a seqNo, excluding the client that made the request
+ *
+ * @param db
+ * @param requestingClientId
+ * @param seqNo
+ * @returns
+ */
+export async function getAllOpsFromSeqNoExclClient(db: DB, requestingClientId: string, seqNo: number): Promise<ServerToClient<Operation>[]> {
+	const operations = await Promise.all([
+		getCardsFromSeqNo(db, requestingClientId, seqNo),
+		getCardContentFromSeqNo(db, requestingClientId, seqNo),
+		getCardDeletedFromSeqNo(db, requestingClientId, seqNo),
+		getDeckFromSeqNo(db, requestingClientId, seqNo),
+		getDeckCardFromSeqNo(db, requestingClientId, seqNo),
+	]);
+
+	return operations.flat().sort((a, b) => a.seqNo - b.seqNo);
+}
---
+++ test/integration/server2client.test.ts
@@ -0,0 +1,174 @@
+import { DB } from '@/db';
+import * as schema from '@/db/schema';
+import {
+	CardContentOperation,
+	CardDeletedOperation,
+	CardOperation,
+	ClientToServer,
+	handleClientOperation,
+	ServerToClient,
+} from '@/operation';
+import { getAllOpsFromSeqNoExclClient } from '@/server2client';
+import { env } from 'cloudflare:test';
+import { eq } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/d1';
+import { createTestUser, testClientId, testClientId2, testUser } from 'test/integration/utils';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+let db: DB;
+beforeEach(async () => {
+	await createTestUser();
+	db = drizzle(env.D1, {
+		schema,
+	});
+});
+
+const now = Date.now();
+
+const cardOp1: ClientToServer<CardOperation> = {
+	type: 'card',
+	userId: testUser.id,
+	clientId: testClientId,
+	timestamp: now,
+	payload: {
+		id: 'test-card-1',
+	},
+};
+
+const cardContentOp1: ClientToServer<CardContentOperation> = {
+	type: 'cardContent',
+	userId: testUser.id,
+	clientId: testClientId,
+	timestamp: now,
+	payload: {
+		cardId: 'test-card-1',
+		front: 'test-front-1',
+		back: 'test-back-1',
+	},
+};
+
+const cardContentOp2FromClient2: ClientToServer<CardContentOperation> = {
+	type: 'cardContent',
+	userId: testUser.id,
+	clientId: testClientId2,
+	timestamp: now + 10000,
+	payload: {
+		cardId: 'test-card-1',
+		front: 'test-front-2',
+		back: 'test-back-2',
+	},
+};
+
+const cardDeletedOp1: ClientToServer<CardDeletedOperation> = {
+	type: 'cardDeleted',
+	userId: testUser.id,
+	clientId: testClientId,
+	timestamp: now,
+	payload: {
+		cardId: 'test-card-1',
+		deleted: false,
+	},
+};
+
+async function addCard() {
+	await handleClientOperation(cardOp1, env.D1);
+	await handleClientOperation(cardContentOp1, env.D1);
+	await handleClientOperation(cardDeletedOp1, env.D1);
+}
+
+describe('server2client', () => {
+	it('should return empty array if no operations', async () => {
+		const operations = await getAllOpsFromSeqNoExclClient(db, 'not-the-client', 0);
+		expect(operations).toHaveLength(0);
+	});
+
+	it('should return empty array if request operations from same client', async () => {
+		await handleClientOperation(cardOp1, env.D1);
+
+		const operations = await getAllOpsFromSeqNoExclClient(db, testClientId, 0);
+		expect(operations).toHaveLength(0);
+	});
+
+	it('should return operations if different client requesting', async () => {
+		await addCard();
+		const operations = await getAllOpsFromSeqNoExclClient(db, 'not-the-client', 0);
+
+		expect(operations).toHaveLength(3);
+		const cardOperation = operations[0] as ServerToClient<CardOperation>;
+		const cardContentOperation = operations[1] as ServerToClient<CardContentOperation>;
+		const cardDeletedOperation = operations[2] as ServerToClient<CardDeletedOperation>;
+
+		expect(cardOperation.type).toBe('card');
+		expect(cardOperation.payload.id).toBe('test-card-1');
+		expect(cardOperation.seqNo).toBe(1);
+
+		expect(cardContentOperation.type).toBe('cardContent');
+		expect(cardContentOperation.payload.cardId).toBe('test-card-1');
+		expect(cardContentOperation.seqNo).toBe(2);
+
+		expect(cardDeletedOperation.type).toBe('cardDeleted');
+		expect(cardDeletedOperation.payload.cardId).toBe('test-card-1');
+		expect(cardDeletedOperation.seqNo).toBe(3);
+	});
+
+	it('should return empty array if after greatest seqNo', async () => {
+		await addCard();
+		const result = await db.query.users.findFirst({
+			where: eq(schema.users.id, testUser.id),
+			columns: {
+				nextSeqNo: true,
+			},
+		});
+
+		if (!result) {
+			throw new Error('Failed to get nextSeqNo');
+		}
+		const nextSeqNo = result.nextSeqNo;
+
+		const operations = await getAllOpsFromSeqNoExclClient(db, 'not-the-client', nextSeqNo);
+		expect(operations).toHaveLength(0);
+	});
+
+	it('should return latest operations in order of seqNo', async () => {
+		await addCard();
+		await handleClientOperation(cardContentOp2FromClient2, env.D1);
+
+		const operations = await getAllOpsFromSeqNoExclClient(db, 'not-the-client', 0);
+
+		expect(operations).toHaveLength(3);
+
+		const cardOperation = operations[0] as ServerToClient<CardOperation>;
+		const cardDeletedOperation = operations[1] as ServerToClient<CardDeletedOperation>;
+		const cardContentOperation = operations[2] as ServerToClient<CardContentOperation>;
+
+		expect(cardOperation.payload.id).toBe('test-card-1');
+		expect(cardOperation.seqNo).toBe(1);
+
+		expect(cardDeletedOperation.type).toBe('cardDeleted');
+		expect(cardDeletedOperation.payload.deleted).toBe(false);
+		expect(cardDeletedOperation.seqNo).toBe(3);
+
+		expect(cardContentOperation.payload.front).toBe('test-front-2');
+		expect(cardContentOperation.payload.back).toBe('test-back-2');
+		expect(cardContentOperation.seqNo).toBe(4);
+	});
+
+	it('should skip some operations if they are from the same client', async () => {
+		await addCard();
+		await handleClientOperation(cardContentOp2FromClient2, env.D1);
+
+		const operations = await getAllOpsFromSeqNoExclClient(db, testClientId2, 0);
+		expect(operations).toHaveLength(2);
+
+		const cardOperation = operations[0] as ServerToClient<CardOperation>;
+		const cardDeletedOperation = operations[1] as ServerToClient<CardDeletedOperation>;
+
+		expect(cardOperation.payload.id).toBe('test-card-1');
+		expect(cardOperation.seqNo).toBe(1);
+
+		expect(cardDeletedOperation.type).toBe('cardDeleted');
+		expect(cardDeletedOperation.payload.deleted).toBe(false);
+		expect(cardDeletedOperation.seqNo).toBe(3);
+	});
+});
+
